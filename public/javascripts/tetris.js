e742d9e64749fd67e77092bb00e89f82e3a57951
Yaki <yaki.koren@gmail.com>
2019-01-20 15:04:12 +0200

    restructuring

---
+++ public/javascripts/tetris.js
@@ -0,0 +1,19 @@
+import { boardMethods } from "./boardMethods.js"
+export var tetris = {
+    run : function (document) {
+        
+        const board = {
+            width: 400,
+            height: 500,
+            rows: 20,
+            columns: 16
+        }
+        var canvas = document.createElement("canvas");
+        document.getElementsByTagName('body')[0].appendChild(canvas);
+        canvas.width = board.width;
+        canvas.height = board.height
+        var ctx = canvas.getContext('2d')
+
+        boardMethods.draw(ctx, board)
+    }
+}
