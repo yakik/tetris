42105ff1876d08083b2137fb20df79b1ae683941
Yaki <yaki.koren@gmail.com>
2019-01-24 17:09:25 +0200

    move checks and move

--- cypress/integration/itemTests.js
+++ cypress/integration/itemTests.js
@@ -1,7 +1,5 @@
-import { setCellValue,getNewBoard, getCellValue, getBoardWithItem, canItemBePlacedOnBoard, getBoardAfterItemRemoved } from "../../public/javascripts/board"
-import { getNewItem,getCoordinatesWithOffset } from "../../public/javascripts/item"
-
-
+import { moveItemOnBoard,isAllowedToMove,setCellValue,getNewBoard, getCellValue, getBoardWithItem, canItemBePlacedOnBoard, getBoardAfterItemRemoved } from "../../public/javascripts/board"
+import { getNewItem } from "../../public/javascripts/item"
 
 describe('board tests', function () {
     it('knows to reflect item on board', function() {
@@ -23,6 +21,69 @@ describe('board tests', function () {
       expect(getCellValue(newBoard,{col:1,row:2})).to.eq(0)
     })
   
+    it('Moves an item', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      newBoard = moveItemOnBoard(newBoard, { col: 1, row: 1 })
+      expect(getCellValue(newBoard,{col:0,row:0})).to.eq(0)
+      expect(getCellValue(newBoard,{col:1,row:0})).to.eq(0)
+      expect(getCellValue(newBoard,{col:0,row:1})).to.eq(0)
+      expect(getCellValue(newBoard,{col:1,row:1})).to.eq(1)
+      expect(getCellValue(newBoard,{col:2,row:0})).to.eq(0)
+      expect(getCellValue(newBoard,{col:2,row:1})).to.eq(1)
+      expect(getCellValue(newBoard,{col:2,row:2})).to.eq(1)
+      expect(getCellValue(newBoard,{col:0,row:2})).to.eq(0)
+      expect(getCellValue(newBoard,{col:1,row:2})).to.eq(1)
+    })
+  
+  
+    it('allows to move item when possible', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      
+      expect(isAllowedToMove(newBoard, { col: 1, row: 0 })).to.be.true
+      
+    })
+  
+    it('doesnt allow to move left when impossible', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      
+      expect(isAllowedToMove(newBoard, { col: -1, row: 0 })).to.be.false
+      
+    })
+  
+    it('doesnt allow to move right when impossible', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      expect(isAllowedToMove(newBoard, { col: +1, row: 0 })).to.be.true
+   
+      expect(isAllowedToMove(newBoard, { col: +2, row: 0 })).to.be.false
+      
+    })
+  
+    it('doesnt allow to move up when impossible', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      
+      expect(isAllowedToMove(newBoard, { col: +0, row: -1 })).to.be.false
+      
+    })
+  
+    it('doesnt allow to move down when impossible', function() {
+      var squareItem = getNewItem('Square')
+      var board = getNewBoard(3,3)
+      var newBoard = getBoardWithItem(board, squareItem, { col: 0, row: 0 })
+      expect(isAllowedToMove(newBoard, { col: +0, row: +1 })).to.be.true
+     
+      expect(isAllowedToMove(newBoard, { col: +0, row: +2 })).to.be.false
+      
+    })
   
     it('knows to check whether item can be placed on board (can)', function() {
       var squareItem = getNewItem('Square')
@@ -30,7 +91,7 @@ describe('board tests', function () {
       var board = getNewBoard(3,3)
       var itemLocationOnBoard = { col: 0, row: 0 }
       
-      debugger
+      
       expect(canItemBePlacedOnBoard(board,squareItem, itemLocationOnBoard)).to.be.true
     })
   
@@ -43,11 +104,8 @@ describe('board tests', function () {
       expect(canItemBePlacedOnBoard(board,squareItem, itemLocationOnBoard)).to.be.false
     })
   
-    
-  
     it('checks whether item in board limit (big x)', function() {
       var squareItem = getNewItem('Square')
-  
       var board = getNewBoard(3,3)
       var itemLocationOnBoard = { col: 2, row: 0 }
       
@@ -56,7 +114,6 @@ describe('board tests', function () {
   
     it('checks whether item in board limit (small x)', function() {
       var squareItem = getNewItem('Square')
-  
       var board = getNewBoard(3,3)
       var itemLocationOnBoard = { col: -1, row: 0 }
       
@@ -65,7 +122,6 @@ describe('board tests', function () {
   
     it('checks whether item in board limit (small y', function() {
       var squareItem = getNewItem('Square')
-  
       var board = getNewBoard(3,3)
       var itemLocationOnBoard = { col: 0, row: -1 }
       
@@ -74,7 +130,6 @@ describe('board tests', function () {
   
     it('checks whether item in board limit (big y)', function() {
       var squareItem = getNewItem('Square')
-  
       var board = getNewBoard(3,3)
       var itemLocationOnBoard = { col: 0, row: 2 }
       
--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -2,16 +2,16 @@
 import { getCoordinatesWithOffset } from "./item.js"
 
 export function canItemBePlacedOnBoard(board, item, itemLocationOnBoard) {
-    if (!isItemWithinBoardLimits(board,  item, itemLocationOnBoard))
+    if (!isItemWithinBoardLimits(board, item, itemLocationOnBoard))
         return false
     else
-        return isItemCoordinatesVacant(board,  item, itemLocationOnBoard);
+        return isItemCoordinatesVacant(board, item, itemLocationOnBoard);
 
 }
 
 function isItemWithinBoardLimits(board, item, itemLocationOnBoard) {
     var itemCoordinates = getCoordinatesWithOffset(item, itemLocationOnBoard)
-    
+
     var returnValue = true;
     itemCoordinates.forEach(coordinate => {
         if (coordinate.col < 0 || coordinate.row < 0 ||
@@ -26,7 +26,7 @@ function isItemCoordinatesVacant(board, item, itemLocationOnBoard) {
     var itemCoordinates = getCoordinatesWithOffset(item, itemLocationOnBoard)
     var returnValue = true;
     itemCoordinates.forEach(coordinate => {
-        if (getCellValue(board,coordinate) != 0)
+        if (getCellValue(board, coordinate) != 0)
             returnValue = false;
     });
     return returnValue;
@@ -38,9 +38,39 @@ export function getBoardWithItem(board, item, itemLocationOnBoard) {
     newBoard.item = item
     newBoard.itemLocationOnBoard = itemLocationOnBoard
     itemCoordinates.forEach(coordinate => {
-        setCellValue(newBoard,coordinate,item.key)
+        setCellValue(newBoard, coordinate, item.key)
     });
     return newBoard
+}
+
+
+export function moveItemOnBoard(board, offest) {
+    var newBoard = removeItemFromBoard(board)
+    var itemLocationOnBoard = {}
+
+    itemLocationOnBoard.col = board.itemLocationOnBoard.col + offest.col
+    itemLocationOnBoard.row = board.itemLocationOnBoard.row + offest.row
+    return getBoardWithItem(newBoard, board.item, itemLocationOnBoard)
+
+}
+
+export function isAllowedToMove(board, offest) {
+    var newBoard = removeItemFromBoard(board)
+    var itemLocationOnBoard = {}
+
+    itemLocationOnBoard.col = board.itemLocationOnBoard.col + offest.col
+    itemLocationOnBoard.row = board.itemLocationOnBoard.row + offest.row
+    return canItemBePlacedOnBoard(newBoard, board.item, itemLocationOnBoard)
+
+}
+
+function removeItemFromBoard(board) {
+    var itemCoordinates = getCoordinatesWithOffset(board.item, board.itemLocationOnBoard)
+    var newBoard = duplicateBoard(board);
+    itemCoordinates.forEach(coordinate => {
+        setCellValue(newBoard, coordinate, 0)
+    });
+    return newBoard;
 }
 
 
