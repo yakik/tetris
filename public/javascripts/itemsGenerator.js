2ed13e3509a9a5c91825ae5ce0fc95d173645502
Yaki <yaki.koren@gmail.com>
2019-01-28 12:26:25 +0200

    new pieces appear on board

--- public/javascripts/itemsGenerator.js
+++ public/javascripts/itemsGenerator.js
@@ -1,5 +1,15 @@
 import { getNewItem } from "./item.js"
 
+var typesArray = [7]
+typesArray[0] = getNewItem('Square',300)
+typesArray[1] = getNewItem('Line',300)
+typesArray[2] = getNewItem('Plus',300)
+typesArray[3] = getNewItem('LeftL',300)
+typesArray[4] = getNewItem('RightL',300)
+typesArray[5] = getNewItem('LeftStep',300)
+typesArray[6] = getNewItem('RightStep',300)
+
+
 export function getNextItem() {
-    return getNewItem('Square',300)
+    return typesArray[Math.floor(Math.random() * 7)]
 }
