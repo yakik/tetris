82dd29e34b7ae400bacb65e2496902c2f646522e
Yaki <yaki.koren@gmail.com>
2019-02-02 22:03:09 +0200

    tetris refactoring

--- cypress/integration/GUITests.js
+++ cypress/integration/GUITests.js
@@ -1,29 +1,103 @@
+var colors = {
+    red: 'rgb(255, 0, 0)',
+    blue: 'rgb(0, 0, 255)',
+    yellow: 'rgb(255, 255, 0)',
+    green: 'rgb(0, 128, 0)',
+    purple: 'rgb(128, 0, 128)',
+    orange: 'rgb(255, 165, 0)',
+    grey: 'rgb(128, 128, 128)'
+}
+
 describe('GUI tests', function () {
     it('test', function () {
 
         cy.visit('http://localhost:3000/?mode=test')
         cy.window().then((win) => {
-            win.onkeypress({ key: 'z', nextTestItem : 'Square' })
-            win.onkeypress({ key: 'm' })
-            win.onkeypress({ key: 'z', nextTestItem: 'Line' })
+            introduceNextItem(win, 'Square');
+            moveItemLeft(win, 10)
+            dropItemDown(win);
             
-            win.onkeypress({ key: 'm' })
+            introduceNextItem(win, 'Line');
+            rotateItemCW(win, 1)
+            moveItemRight(win, 10)
+            dropItemDown(win);
+           
+            introduceNextItem(win, 'Plus');
+            moveItemRight(win, 4)
+            dropItemDown(win);
+
+            introduceNextItem(win, 'LeftL');
+            moveItemLeft(win, 2)
+            dropItemDown(win);
+
+            introduceNextItem(win, 'RightL');
+            moveItemRight(win, 1)
+            dropItemDown(win);
+
+            introduceNextItem(win, 'LeftStep');
+            moveItemRight(win, 1)
+            rotateItemCW(win, 1)
+            dropItemDown(win);
+
+            introduceNextItem(win, 'RightStep');
+            moveItemRight(win, 4)
+            rotateItemCW(win, 1)
+            dropItemDown(win);
+
+            // console.log()
+
+
+            assertCellBackgroundColor('#myTD_18_0', 'yellow'); 
+            assertCellBackgroundColor('#myTD_18_2', 'red');  
+            assertCellBackgroundColor('#myTD_18_6', 'blue');
+            assertCellBackgroundColor('#myTD_18_11', 'green');
+            assertCellBackgroundColor('#myTD_18_10', 'grey');
+            assertCellBackgroundColor('#myTD_18_9', 'purple');
+            assertCellBackgroundColor('#myTD_18_7','orange');
             
-            win.onkeypress({ key: 'z', nextTestItem : 'Plus' })
-            win.onkeypress({ key: 'm' })
-            win.onkeypress({ key: 'z', nextTestItem : 'LeftL' })
-            win.onkeypress({ key: 'm' })
-            win.onkeypress({ key: 'z', nextTestItem : 'RightL' })
-            win.onkeypress({ key: 'm' })
-            win.onkeypress({ key: 'z', nextTestItem : 'LeftStep' })
-            win.onkeypress({ key: 'm' })
-            win.onkeypress({ key: 'z', nextTestItem : 'RightStep' })
-            win.onkeypress({ key: 'm' })
+        
           })
         
         
     })
 })
+function assertCellBackgroundColor(cell,myColor) {
+    cy.get(cell).then(cell => {
+        expect(cell).to.have.css('background-color',colors[myColor] );
+    });
+}
+
+function moveItemLeft(win, steps) {
+    for (var i = 0; i < steps;i++)
+        win.onkeypress({ key: 'j' });
+}
+
+function moveItemRight(win, steps) {
+    for (var i = 0; i < steps;i++)
+        win.onkeypress({ key: 'l' });
+}
+
+function rotateItemCW(win, rotations) {
+    for (var i = 0; i < rotations;i++)
+        win.onkeypress({ key: 'k' });
+}
+
+function rotateItemCCW(win, rotations) {
+    for (var i = 0; i < rotations;i++)
+        win.onkeypress({ key: 'i' });
+}
 
 
+function dropItemDown(win) {
+    win.onkeypress({ key: 'm' });
+}
+
+function moveItemDown(win,steps) {
+    for (var i = 0; i < steps;i++)
+        win.onkeypress({ key: 'z' });
+}
+
+function introduceNextItem(win, itemType) {
+    win.onkeypress({ key: 'x', nextTestItem: itemType  });
+}
       
--- public/javascripts/tetris.js
+++ public/javascripts/tetris.js
@@ -1,109 +1,82 @@
 import {
+    canItemBePlacedOnBoard,
     getNewBoard, getBoardWithItem, isAllowedToMove, getBoardAfterMovingItem, getBoardAfterItemCWRotation,
-    getBoardAfterItemCCWRotation    , isAllowedToRotateCW, isAllowedToRotateCCW} from "./board.js"
-import { redraw , setBoardDimensions, drawBoard} from "./boardDrawer.js"
+    getBoardAfterItemCCWRotation, isAllowedToRotateCW, isAllowedToRotateCCW
+} from "./board.js"
+import { redraw, setBoardDimensions, drawBoard } from "./boardDrawer.js"
 import { getBoardCurrentInterval, setBoard, updateBoard, getBoard } from "./gameBoard.js"
-import {getNewItem} from "./item.js"
+import { getNewItem } from "./item.js"
 
 
-var  nextTimedEvent, startCol, runMode, getNextItem
+var nextTimedEvent, startCol, runMode, getNextItem
 
 function updateBoardAndRedraw(fromBoard) {
     updateBoard(fromBoard)
     redraw(getBoard())
 }
 
-
-
 export function keyWasPressed(e) {
-    
-    if ((e.key) == 'j') {
-        if (isAllowedToMove(getBoard(), { col: -1, row: 0 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: -1, row: 0 }))
-        }
-    }
-    if (e.key == 'l') {
-        if (isAllowedToMove(getBoard(), { col: +1, row: 0 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: +1, row: 0 }))
-        }
-    }
-    if (e.key == 'k') {
-  if (isAllowedToRotateCW(getBoard())) {
-            updateBoardAndRedraw(getBoardAfterItemCWRotation(getBoard()))
-        }
-    }
-    if (e.key == 'i') {
-        if (isAllowedToRotateCCW(getBoard())) {
-                  updateBoardAndRedraw(getBoardAfterItemCCWRotation(getBoard()))
-              }
-          }
-
-    if (e.key == 'm') {
-        while (isAllowedToMove(getBoard(), { col: 0, row: +1 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: 0, row: +1 }))
-        }
-    }
-    if (e.key == 'z') {
-        updateBoardAndRedraw(getBoardWithItem(getBoard(),
-            getNewItem(e.nextTestItem, 300), { col: startCol, row: 0 }))
+    switch (e.key) {
+        case 'j':
+            if (isAllowedToMove(getBoard(), { col: -1, row: 0 }))
+                updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: -1, row: 0 }))
+            break
+        case 'l':
+            if (isAllowedToMove(getBoard(), { col: +1, row: 0 }))
+                updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: +1, row: 0 }))
+            break
+        case 'k':
+            if (isAllowedToRotateCW(getBoard()))
+                updateBoardAndRedraw(getBoardAfterItemCWRotation(getBoard()))
+            break
+        case 'i':
+            if (isAllowedToRotateCCW(getBoard()))
+                updateBoardAndRedraw(getBoardAfterItemCCWRotation(getBoard()))
+            break
+        case 'm':
+            while (isAllowedToMove(getBoard(), { col: 0, row: +1 }))
+                updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: 0, row: +1 }))
+            break
+        case 'z':
+            if (isAllowedToMove(getBoard(), { col: 0, row: +1 }))
+                updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: 0, row: +1 }))
+            break
+        case 'x':
+            if (e.nextTestItem != null)
+                addNewItem(getNewItem(e.nextTestItem, 300))
     }
 }
 
 export function tetris(document, myBoardConfig, myStartCol, myGetNextItem, myRunMode) {
-  
     runMode = myRunMode
-    
     startCol = myStartCol
-
     getNextItem = myGetNextItem
 
-   
     setBoardDimensions(myBoardConfig.boardWidth, myBoardConfig.boardHeight)
-    
-    setBoard(getNewBoard(myBoardConfig.boardColumns, myBoardConfig.boardRows))
-    drawBoard( myBoardConfig.boardRows,myBoardConfig.boardColumns,getBoard())
-    updateBoardAndRedraw(getBoardWithItem(getBoard(), getNextItem(), { col: startCol, row: 0 }))
 
+    setBoard(getNewBoard(myBoardConfig.boardColumns, myBoardConfig.boardRows))
+    drawBoard(myBoardConfig.boardRows, myBoardConfig.boardColumns, getBoard())
     window.onkeypress = keyWasPressed
-    if (runMode==="prod")
+    
+    if (runMode === "prod") {
+        addNewItem(getNextItem());
         nextTimedEvent = setTimeout(moveDownOneAfterInterval, getBoardCurrentInterval())
-    document.addEventListener("click", mouseclicked)
+    }
 }
 
-
+function addNewItem(newItem) {
+    if (canItemBePlacedOnBoard(getBoard(), newItem, { col: startCol, row: 0 }))
+        updateBoardAndRedraw(getBoardWithItem(getBoard(), newItem, { col: startCol, row: 0 }))
+    else
+        alert("game over.")
+}
 
 export function moveDownOneAfterInterval() {
     if (isAllowedToMove(getBoard(), { col: 0, row: 1 })) {
         updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: 0, row: 1 }))
     }
     else {
-        updateBoardAndRedraw(getBoardWithItem(getBoard(), getNextItem(), { col: startCol, row: 0 }))
+        addNewItem(getNextItem())
     }
     nextTimedEvent = setTimeout(moveDownOneAfterInterval, getBoardCurrentInterval())
 }
-
-export function mouseclicked(event) {
-
-    if (event.clientY > 350) {
-        while (isAllowedToMove(getBoard(), { col: 0, row: +1 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: 0, row: +1 }))
-        }
-    }
-
-    if (event.clientY <100) {
-        if (isAllowedToRotateCW(getBoard())) {
-            updateBoardAndRedraw(getBoardAfterItemCWRotation(getBoard()))
-        }
-    }
-    if (event.clientX < 200) {
-        if (isAllowedToMove(getBoard(), { col: -1, row: 0 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: -1, row: 0 }))
-        }
-    }
-    else {
-        if (isAllowedToMove(getBoard(), { col: +1, row: 0 })) {
-            updateBoardAndRedraw(getBoardAfterMovingItem(getBoard(), { col: +1, row: 0 }))
-        }
-    }
-}
-
